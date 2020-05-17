83a21ce71293b6cd14290b0e5b64ac78ab1a72e9
HenriqueMiossi <henriquemiossi@gmail.com>
2020-05-17 16:37:27 -0300

    Adicionada tela de erro com botão voltar

--- scripts/upload.php
+++ scripts/upload.php
@@ -11,33 +11,33 @@ if(isset($_POST["submit"])) {
     echo "Arquivo é uma imagem - " . $check["mime"] . ". ";
     $uploadOk = 1;
   } else {
-    echo "Arquivo não é uma imagem. ";
     $uploadOk = 0;
+    header("location: ../error.html");
   }
 }
 
 // Checa se o arquivo já existe
 if (file_exists($target_file)) {
-  echo "Desculpe, o arquivo já existe. ";
   $uploadOk = 0;
+  header("location: ../error.html");
 }
 
 // Permite certos tipos de arquivos
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-  echo "Somente arquivos JPG, JPEG e PNG são permitidos. ";
   $uploadOk = 0;
+  header("location: ../error.html");
 }
 
 // Checa se $uploadOk foi configurado como 0 por um erro
 if ($uploadOk == 0) {
-  echo "Desculpe, seu arquivo não foi enviado. ";
+  header("location: ../error.html");
 // Se tudo estiver OK, envia o arquivo
 } else {
   if (move_uploaded_file($_FILES["inputFile"]["tmp_name"], $target_file)) {
     echo "O arquivo ". basename( $_FILES["inputFile"]["name"]). " foi enviado. ";
     header("location: ../convert.html");
   } else {
-    echo "Desculpe, houve um erro ao enviar seu arquivo. ";
+    header("location: ../error.html");
   }
 }
 ?>
